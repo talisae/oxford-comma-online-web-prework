77f1a762dc819d966fe465480aa09699c6c7359f
Talisa Erazo <talisaerazo@gmail.com>
2018-10-18 13:58:36 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,15 +3,3 @@ def oxford_comma(array)
      array.join("and")
   end
 end
-
-  
-  def oxford_comma(array)
-     if array.length < 2
-  end 	    array.join
-    elsif array.length == 2
-      array.insert(1, "and").join(" ")
-    else
-      str = ", and " + array.pop
-      str = array.join(", ") << str
-    end
-  end
