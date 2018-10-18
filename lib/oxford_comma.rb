33aea82aaa0af24d7a3122c1b94a3df1f69c8042
Talisa Erazo <talisaerazo@gmail.com>
2018-10-18 14:06:30 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -9,15 +9,3 @@ def oxford_comma(array)
      array.join(",")
   end
 end
-
-
-  if array.length == 2
-  array[-2] << " and "
-  array.join
-  elsif array.length == 1
-  	array.join
-  elsif array.length > 2
-    array[-1].prepend "and "
-	array.join(", ")
-  end
-end
